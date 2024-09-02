6d682313635331f254e543b59c9255374ffd183e
YoGeSh <yogeshrathee7400@gmail.com>
2024-09-02 09:42:23 +0530

    first commit

---
+++ src/main/java/org/example/magicalarenagamerun/MagicalArenaGameRunApplication.java
@@ -0,0 +1,13 @@
+package org.example.magicalarenagamerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicalArenaGameRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicalArenaGameRunApplication.class, args);
+    }
+
+}
