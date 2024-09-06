ba81b61a0d3cf0487cdb2caa6b68de6f3ca1ca77
YoGeSh <yogeshrathee7400@gmail.com>
2024-09-06 17:03:09 +0530

    Add

---
+++ src/main/java/org/example/magicalarenagamerun/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package org.example.magicalarenagamerun.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Magical-Arena-Game")
+                        .description("Restful APIs")
+                        .contact(new Contact()
+                                .name("Developer")
+                                .url("https://yogeshrathee.github.io/combined-portflio/")));
+    }
+}
